1fac14fdca3b51b70210b953185f67dde64fb473
Lars Reuter <lp.reuter@gmail.com>
2024-01-18 19:38:40 +0100

    Fetch additional data points via API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import WeatherDisplayComponent from './WeatherDisplayComponent';
 
 export default function App() {
-  const initialWeatherDescription = '';
-  const [weatherDescription, setWeatherDescription] = useState(
-    initialWeatherDescription,
-  );
+  // Const for API secret.
+  const APIkey = process.env.REACT_APP_openWeatherAPIKey;
+  // Define states for required weather data points.
+  const [weatherDescription, setWeatherDescription] = useState('');
+  const [locationName, setLocationName] = useState('');
+  // Fetch weather data.
+  useEffect(() => {
+    const weatherData = async () => {
+      const fetchWeatherData = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${APIkey}`,
+      );
+      const weatherDataJson = await fetchWeatherData.json();
+      const weather = await weatherDataJson;
+      setWeatherDescription(weather.weather[0].description);
+      setLocationName(weather.name);
+    };
+    weatherData();
+    return () => {};
+  }, []);
+  // End of fetch weather data.
   return (
     <>
-      <div>Hallo</div>
-      <p>Test</p>
-      <WeatherDisplayComponent
-        weatherDescription={weatherDescription}
-        setWeatherDescription={setWeatherDescription}
-      />
-      {/* <p>{weather.weather[0]}</p> */}
-      <p>{weatherDescription}</p>
+      <p>{`Status: ${weatherDescription}`}</p>
+      <p>{`Location: ${locationName}`}</p>
     </>
   );
 }
