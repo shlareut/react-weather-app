a0fba5a67fb6ea90548a2cae8367b26fc305c9ab
Lars Reuter <lp.reuter@gmail.com>
2024-01-18 19:19:00 +0100

    Add weather API POC

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
+import WeatherDisplayComponent from './WeatherDisplayComponent';
 
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const initialWeatherDescription = '';
+  const [weatherDescription, setWeatherDescription] = useState(
+    initialWeatherDescription,
+  );
+  return (
+    <>
+      <div>Hallo</div>
+      <p>Test</p>
+      <WeatherDisplayComponent
+        weatherDescription={weatherDescription}
+        setWeatherDescription={setWeatherDescription}
+      />
+      {/* <p>{weather.weather[0]}</p> */}
+      <p>{weatherDescription}</p>
+    </>
+  );
 }
